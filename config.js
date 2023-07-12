693fbc524afc48cf2d94d6bae7899c1dbd283ccc
Umair <100330105+Umairamee@users.noreply.github.com>
2023-07-12 14:53:57 +0500

    Update config.js

--- config.js
+++ config.js
@@ -22,23 +22,23 @@ global.lApiKey = {// if this API key get expired then please go to https://api.l
 }
 
 
-global.botname = "Queen Anya" 
-global.author = "Enter Author Name Here"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "Enter Your Any Website Profile Link Here"
-global.footer = "©\t" + "Enter Your Watermark Here"
+global.botname = "Maher G" 
+global.author = "https://youtube.com/@srtechnicalraj2984"  
+global.packname = "𝙌𝙐𝙀𝙀𝙉✯MAHER✯𝘽𝙊𝙏"  
+global.myweb = "https://youtube.com/@srtechnicalraj2984"
+global.footer = "©\t" + "Maher baba"
 global.prefa = ['-'] // single prefix
 global.themeemoji = "🎐"
 global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
 global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
-global.ytname = "YT: Enter Your Channel Name Here"
-global.continent = "Asia" // your continent name 
-global.region = "Enter Your Country Name Here"
-global.state = "Enter Your State Name Here" // your state name
+global.ownername = "Maher baba "    
+global.ownernumber = ["923034830074"]
+global.ytname = "YT:https://youtube.com/@srtechnicalraj2984"
+global.continent = "Jani" // your continent name 
+global.region = "Pakistan "
+global.state = "jani don" // your state name
 global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
 global.instagramId = "Enter Your Insta ID Here"
 global.email = "Enter Your Email ID Here"
